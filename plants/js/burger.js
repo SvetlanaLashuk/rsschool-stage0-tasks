4b30788b33f2ad440ac91b6caa73a8ba174c71a9
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-01-17 21:15:29 +0300

    feat: modify burger menu

--- plants/js/burger.js
+++ plants/js/burger.js
@@ -1,14 +1,18 @@
 
+const body = document.querySelector("body");
 const burgerIcon    = document.querySelector('.burger');
 const menu          = document.querySelector('.header__nav');
 const menuCloseItem = document.querySelector('.header__nav-close');
 
 burgerIcon.addEventListener('click', () => {
     menu.classList.add('active');
+    body.classList.add('lock');
 });
 menuCloseItem.addEventListener('click', () => {
     menu.classList.remove('active');
+    body.classList.add('lock');
 });
 menu.addEventListener('click', () => {
     menu.classList.remove('active');
-  });
+    body.classList.remove('lock');
+});

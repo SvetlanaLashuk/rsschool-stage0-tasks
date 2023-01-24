f115f62f8a9719733837c32a03684cf702cada72
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-01-24 16:28:04 +0300

    feat: modify blur effect

--- plants/js/blur.js
+++ plants/js/blur.js
@@ -1,123 +1,82 @@
-const serviceButtons = document.querySelectorAll('.service__button');
-const buttons = document.querySelector('.service__btn-group');
-const serviceItems = document.querySelectorAll('.service__item');
-buttons.addEventListener('click', (event) => {
-    serviceButtons.forEach((button, index) => {
-        if (button === event.target) {
-            // if (button.classList.contains('active')) {
-            //     button.classList.remove('active');
-            // }
-            //alert(event.currentTarget);
-            if (button.classList.contains('active')) {
-                // const serviceItemsIsBlur = document.querySelectorAll('.service__item.blur');
-                // serviceItemsIsBlur.forEach((serviceItemIsBlur) => {
-                //     serviceItemIsBlur.classList.remove('blur');
-                // });
-                serviceItems.forEach((serviceItem) => {
-                    if (button.textContent.includes('Garden')) {
-                        if (serviceItem.textContent.includes('Garden')) {
-                            serviceItem.classList.remove('blur');
-                        }
-                    } else if (button.textContent.includes('Planting')) {
-                        if (serviceItem.textContent.includes('Planting')) {
-                            serviceItem.classList.remove('blur');
-                        }
-                    } else if (button.textContent.includes('Lawn')) {
-                        if (serviceItem.textContent.includes('Lawn')) {
-                            serviceItem.classList.remove('blur');
-                        }
-                    }
-                });
-                button.classList.remove('active');
+const buttons           = document.querySelector('.service__btn-group');
+const serviceButtons    = document.querySelectorAll('.service__button');
+const serviceItems      = document.querySelectorAll('.service__item');
+let buttonsClickCounter = 0;
+let isActive = false;
+let buttonTitle;
 
-            } else {
-                
+buttons.addEventListener('click', (event) => {
+    serviceButtons.forEach((serviceButton) => {
+        if (serviceButton === event.target) {
+            buttonTitle = serviceButton.textContent.trim().slice(0, length - 1);
+
+            function addBlurItem(title) {
                 serviceItems.forEach((serviceItem) => {
-                    if (button.textContent.includes('Garden')) {
-                        alert(index);
-                        if (index > 0) {
-                            if (serviceItem.textContent.includes('Garden')) {
-                                serviceItem.classList.remove('blur');
-                            }
-                        } else {
-                            if (!serviceItem.textContent.includes('Garden')) {
-                                serviceItem.classList.add('blur');
-                            }
+                    if (serviceButton.textContent.includes(title)) {
+                        if (!serviceItem.textContent.includes(title)) {
+                            serviceItem.classList.add('blur');
                         }
-                    } else if (button.textContent.includes('Planting')) {
-                        alert(index);
-                        if (index > 0) {
-                            if (serviceItem.textContent.includes('Planting')) {
+                    }
+                });  
+            }
+
+            function removeBlurItem(title, isActive) {
+                serviceItems.forEach((serviceItem) => {
+                    if (serviceButton.textContent.includes(title)) {
+                        if (isActive) {
+                            if (!serviceItem.textContent.includes(title)) {
                                 serviceItem.classList.remove('blur');
-                            }
+                            } 
                         } else {
-                            if (!serviceItem.textContent.includes('Planting')) {
-                                serviceItem.classList.add('blur');
-                            }
-                        }
-                    } else if (button.textContent.includes('Lawn')) {
-                        alert(index);
-                        if (index > 0) {
-                            if (serviceItem.textContent.includes('Lawn')) {
+                            if (serviceItem.textContent.includes(title)) {
                                 serviceItem.classList.remove('blur');
-                            }
-                        } else {
-                            if (!serviceItem.textContent.includes('Lawn')) {
-                                serviceItem.classList.add('blur');
                             }
                         }
                     }
-                });
-                button.classList.add('active');
+                });  
+            }
+
+            function removeAllActiveButtons() {
+                let serviceButtonsIsActive = document.querySelectorAll('.service__button.active')
+                serviceButtonsIsActive.forEach((serviceButtonIsActive) => {
+                    serviceButtonIsActive.classList.remove('active');
+                });  
+            }
+
+            function removeAllBlurItems() {
+                const serviceItemsIsBlur = document.querySelectorAll('.service__item.blur');
+                serviceItemsIsBlur.forEach((serviceItemIsBlur) => {
+                    serviceItemIsBlur.classList.remove('blur');
+                }); 
+            }
+
+            if (serviceButton.classList.contains('active')) {
+                isActive = true;
+                removeBlurItem(buttonTitle, isActive); 
+                serviceButton.classList.remove('active');
+            } else {
+                if (serviceButton) {
+                     let serviceButtonsIsActive = document.querySelectorAll('.service__button.active');
+                     if (serviceButtonsIsActive.length !== 0 || buttonsClickCounter === 0) {
+                        buttonsClickCounter += 1;
+                     }
+                }
+                if (buttonsClickCounter === serviceButtons.length) {
+                    removeAllActiveButtons();
+                    removeAllBlurItems();
+                    addBlurItem(buttonTitle);
+                    serviceButton.classList.add('active');
+                    buttonsClickCounter = 1;
+                } else if (buttonsClickCounter > 1) {
+                    isActive = false;
+                    removeBlurItem(buttonTitle, isActive);
+                    serviceButton.classList.add('active');
+                    
+                } else {
+                    addBlurItem(buttonTitle);
+                    serviceButton.classList.add('active');
+                }
             }
         }
     });
 });
-
-
-
-// serviceButtons.forEach((serviceButton) => {
-//     serviceButton.addEventListener('click', () => {
-//         const serviceItemsIsBlur = document.querySelectorAll('.service__item.blur');
-//         serviceItemsIsBlur.forEach((serviceItemIsBlur, i) => {
-//             // alert(i);
-//             if (i === 2) {
-//                 serviceItemIsBlur.classList.remove('blur'); 
-//             } 
-//             //serviceItemIsBlur.classList.remove('blur');
-//         });
-
-//         if (serviceButton.classList.contains('active')) {
-//             serviceButton.classList.remove('active');
-//         } else {
-//             const serviceButtonsIsActive = document.querySelectorAll('.service__button.active');
-//             serviceButtonsIsActive.forEach((serviceButtonIsActive, i) => {
-//                 alert(serviceButtonIsActive);
-//                 if (i > 2) {
-//                     serviceButtonIsActive.classList.remove('active'); 
-//                 } 
-//                 //serviceButtonIsActive.classList.remove('active');
-//             });
-
-//             serviceButton.classList.add('active');
-
-//             const serviceItems = document.querySelectorAll('.service__item');
-//             serviceItems.forEach((serviceItem) => {
-//                 if (serviceButton.textContent.includes('Garden')) {
-//                     if (!serviceItem.textContent.includes('Garden')) {
-//                         serviceItem.classList.add('blur');
-//                     }
-//                 } else if (serviceButton.textContent.includes('Planting')) {
-//                     if (!serviceItem.textContent.includes('Planting')) {
-//                         serviceItem.classList.add('blur');
-//                     }
-//                 } else if (serviceButton.textContent.includes('Lawn')) {
-//                     if (!serviceItem.textContent.includes('Lawn')) {
-//                         serviceItem.classList.add('blur');
-//                     }
-//                 }
-
-//             });
-//         }
-//     });
-// });

951bae23437e8d8d50f19505e44ad0b8282eb0c2
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-01-27 15:38:16 +0300

    feat: modify prices dropdown

--- plants/js/pricesAccordion.js
+++ plants/js/pricesAccordion.js
@@ -1,10 +1,10 @@
-const priceTypes        = document.querySelectorAll('.price-type');
+const priceTypes = document.querySelectorAll('.price-type');
 
 priceTypes.forEach((priceType) => {
     priceType.addEventListener('click', () =>{
         if (priceType.classList.contains('active')) {
             priceType.classList.remove('active');
-            priceType.parentElement.querySelector('.price-button').classList.remove('active');
+            priceType.parentElement.querySelector('.accordion-button').classList.remove('active');
         } else {
             const priceTypesIsActive = document.querySelectorAll('.price-type.active');
             priceTypesIsActive.forEach((priceTypeIsActive) => {
@@ -17,7 +17,7 @@ priceTypes.forEach((priceType) => {
             });
 
             priceType.classList.add('active'); 
-            priceType.parentElement.querySelector('.price-button').classList.add('active');
+            priceType.parentElement.querySelector('.accordion-button').classList.add('active');
         }
     });
 });
